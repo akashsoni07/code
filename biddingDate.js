e5d7d6364f530122647b3b7008954d93c1c2fa15
akashsoni21 <64322836+akashsoni21@users.noreply.github.com>
2021-05-11 14:49:44 +0530

    first commit

---
+++ biddingDate.js
@@ -0,0 +1,13 @@
+const rp = require("request-promise");
+const cheerio = require("cheerio");
+const url =
+  "https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020";
+
+rp(url)
+  .then((html) => {
+    const $ = cheerio.load(html);
+    console.log($(" .field-content")[69].childNodes[18].childNodes[1].data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
